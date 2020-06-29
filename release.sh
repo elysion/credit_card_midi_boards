098bb4a5fa3babbf82ccbbce2559a671fd0aaae0
Miko Kiiski <miko.kiiski@reaktor.fi>
2020-06-29 19:49:42 +0300

    Fix release script

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 set -e
 
-BOARDS_DIR=$(dirname "$0")
+BOARDS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VERSION=$1
 
 if [ -z ${VERSION} ]; then
@@ -14,25 +15,25 @@ else
 	echo "Building version ${VERSION}"
 fi
 
+echo "Building in ${BOARDS_DIR}"
+
 ZIP_FILE_NAME="credit_card_midi-${VERSION}.zip"
-mv ${BOARDS_DIR}/hardware/* "${BOARDS_DIR}/hardware/${VERSION}"
-git add "${BOARDS_DIR}/hardware/${VERSION}"
+git mv ${BOARDS_DIR}/hardware/* "${BOARDS_DIR}/hardware/${VERSION}"
 cd "${BOARDS_DIR}/hardware"; zip -r "${BOARDS_DIR}/../${ZIP_FILE_NAME}" "${VERSION}"; cd -
-SHA=$(shasum -a 256 "${BOARDS_DIR}/../${ZIP_FILE_NAME}")
+SHA=$(shasum -a 256 "${BOARDS_DIR}/../${ZIP_FILE_NAME}" | awk '{print $1}')
 SIZE=$(stat -f "%z" "${BOARDS_DIR}/../${ZIP_FILE_NAME}")
 
 echo "SHA: ${SHA}, zip size: ${SIZE}"
 
 PACKAGE_FILE="${BOARDS_DIR}/package_credit_card_midi_index.json"
+
+echo "Updating package file ${PACKAGE_FILE}"
 sed -e "s/\([0-9]*\.[0-9]*\.[0-9]*\)/${VERSION}/g" -i "" "${PACKAGE_FILE}"
-sed -e 's/"SHA\-256\:\(.*\)"/"SHA\-256\:${SHA}"/' -i "" "${PACKAGE_FILE}"
-sed -e 's/\("size"\: \)"\([0-9]*\)"/\1"${SIZE}"/' -i "" "${PACKAGE_FILE}"
+sed -e "s/SHA\-256\:\(.*\)\\\"/SHA\-256\:${SHA}\\\"/" -i "" "${PACKAGE_FILE}"
+sed -e "s/\(\\\"size\\\"\: \)\\\"\([0-9]*\)\\\"/\1\\\"${SIZE}\\\"/" -i "" "${PACKAGE_FILE}"
 
 git add "${PACKAGE_FILE}"
 git commit -m "Release v${VERSION}"
 git tag "v${VERSION}"
-
-#echo Updating version to v${NEW_VERSION}
-#sed -e 's/\(<text \)\(.*v1\.0\.0\)/\1locked="yes" \2/' -i "" *.brd
-#sed -e 's/\(<text locked=\"yes\".*v\)\(\d+\.\d+\.\d+\)/\1${NEW_VERSION}/' -i "" ./eagle/*.brd
-#sed -e "s/\(<text locked=\"yes\".*\)\(v[0-9]*\.[0-9]*\.[0-9]*\)/\1v${NEW_VERSION}/" -i "" eagle/*.brd
+git push --tags
+hub release create v${VERSION} -a "${BOARDS_DIR}/../${ZIP_FILE_NAME}" -m "Release v${VERSION}"
